626ff6e5197c9b3dc80ec1b36661cbf5db1a185d
frogstrosity <36r036@gmail.com>
2020-09-04 19:23:41 +0000

    adding a basic blog component passing props to a component..pretty simple and not useful in reality but we learning

--- blogstrosity/src/App.js
+++ blogstrosity/src/App.js
@@ -9,6 +9,16 @@ function Header() {
     </div>
   );
 }
+
+function Post(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <h3>{props.tagline}</h3>
+      <p>{props.text}</p>
+    </div>
+  );
+}
 function PlaceholderCrap() {
   return (
     <div className="App">
@@ -31,9 +41,13 @@ function PlaceholderCrap() {
 }
 
 function App() {
+  const title = "How (Not) to Blog", 
+        tagline ="There are a lot of good reasons to blog, this is not one of them",
+        text="For a while now I have wanted to come up with a project to learn code in my spare time, I also always wanted to come up with a reason to blog. If I learn to crate my own blog and write about that I might fulfill both these goals. It also might just end up being a monstrosity. Either way I hope to learn someting on the way";
   return (
     <div>
       <Header/>
+      <Post title = {title} tagline = {tagline} text={text}/>
       <PlaceholderCrap/>
     </div>
   );
