53fe71638fe5012dfa0b6961bf6e099f3ae4261c
frogstrosity <46494518+frogstrosity@users.noreply.github.com>
2020-09-16 11:24:22 +0000

    add read more with hook... to expand a post and collapse all otehr posts on the page

--- blogstrosity/src/App.js
+++ blogstrosity/src/App.js
@@ -2,46 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import * as PostService from './Services/PostService';
 
-function Header() {
-  return (
-    <div className="Header">
-      <h1>blogstrosity</h1>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div className="Footer">
-      <p>2020 GeoTech</p>
-    </div>
-  );
-}
-
-function Posts({posts}) {
-  const [activeIndex] = useState(1);
-  return (
-    
-    /* To-Do: try to get spread operator working here so we dont need to deconstruct/pass indevidual props */
-    <div className="Posts">
-      {posts.map((p, index) => (<Post post={p} index = {index} expanded = {activeIndex === index}/>))}
-    </div>
-  );
-}
-
-function Post({post, index, expanded}) {
-  
-  return (
-    <div className="Post">
-      <p>{index}</p>
-      <h2 className="Post-Title">{post.title}</h2>
-      <h3>{post.tagline}</h3>
-      <p>{post.text.substring(0, 100).concat('...')}</p>
-      {expanded ? <p>{post.additionalInfo} </p> : null}
-    </div>
-  );
-}
-
 function App() {
   const [data] = useState(PostService.getPosts());
    
@@ -50,6 +10,50 @@ function App() {
       <Header/>
       <Posts posts={data}/>
       <Footer/>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>blogstrosity</h1>
+    </div>
+  );
+}
+
+function Posts({posts}) {
+  const [activeIndex, setActiveIndex] = useState(null);
+  return (
+    
+    /* To-Do: try to get spread operator working here so we dont need to deconstruct/pass indevidual props */
+    <div className="Posts">
+      {posts.map((p, index) => 
+        (<Post post={p} index = {index} expanded = {activeIndex === index} setActiveIndex = {setActiveIndex}/>))}
+    </div>
+  );
+}
+
+function Post({post, index, expanded, setActiveIndex}) {
+  
+  return (
+    <div className="Post">
+      <h2 className="Post-Title">{post.title}</h2>
+      <h3>{post.tagline}</h3>
+      <p>{post.text.substring(0, 100).concat('...')}</p>
+      { 
+        expanded ? 
+        <p>{post.additionalInfo} </p> : 
+        <button onClick={() => setActiveIndex(index)}>Read More...</button>
+      }
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p>2020 GeoTech</p>
     </div>
   );
 }
