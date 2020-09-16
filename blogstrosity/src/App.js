63bdc14534bd53c5a1e451f7aad95cd9b99881cb
geo <46494518+frogstrosity@users.noreply.github.com>
2020-09-16 11:29:48 +0000

    Feature/learn react (#12)
    * add class names
    
    * add todo: try to get spread operator working to simplify code
    
    * use correct commenting...
    
    * just add padding...
    
    want to try and commit everyday, didnt have much time today and i wasnt it trying to get js spread operator working....
    
    * use state to persist posts
    
    * add footer
    
    * start to add additional content section
    with display based on active index state ready for expand/collapse
    
    * add read more with hook...
    to expand a post and collapse all otehr posts on the page

--- blogstrosity/src/App.js
+++ blogstrosity/src/App.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 import * as PostService from './Services/PostService';
 
+function App() {
+  const [data] = useState(PostService.getPosts());
+   
+  return (
+    <div>
+      <Header/>
+      <Posts posts={data}/>
+      <Footer/>
+    </div>
+  );
+}
+
 function Header() {
   return (
     <div className="Header">
@@ -19,37 +31,40 @@ function Footer() {
 }
 
 function Posts({posts}) {
-  const [activeIndex] = useState(1);
+  const [activeIndex, setActiveIndex] = useState(null);
   return (
     
     /* To-Do: try to get spread operator working here so we dont need to deconstruct/pass indevidual props */
     <div className="Posts">
-      {posts.map((p, index) => (<Post post={p} index = {index} expanded = {activeIndex === index}/>))}
+      {posts.map((p, index) => 
+        (<Post post={p} index = {index} expanded = {activeIndex === index} setActiveIndex = {setActiveIndex}/>))}
     </div>
   );
 }
 
-function Post({post, index, expanded}) {
-  
+
+function Post({post, index, expanded, setActiveIndex}) { 
   return (
     <div className="Post">
       <p>{index}</p>
       <h2 className="Post-Title">{post.title}</h2>
       <h3>{post.tagline}</h3>
       <p>{post.text.substring(0, 100).concat('...')}</p>
-      {expanded ? <p>{post.additionalInfo} </p> : null}
+
+      { 
+        expanded ? 
+        <p>{post.additionalInfo} </p> : 
+        <button onClick={() => setActiveIndex(index)}>Read More...</button>
+      }
     </div>
   );
 }
 
-function App() {
-  const [data] = useState(PostService.getPosts());
-   
+
+function Footer() {
   return (
-    <div>
-      <Header/>
-      <Posts posts={data}/>
-      <Footer/>
+    <div className="Footer">
+      <p>2020 GeoTech</p>
     </div>
   );
 }
--- blogstrosity/src/Services/PostService.js
+++ blogstrosity/src/Services/PostService.js
@@ -6,6 +6,10 @@ const data = [
     { title: "How (not) to Learn React", tagline: "doing the frontends", 
       text: "adding basic static layout using components",
       additionalInfo: "lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    { title: "How (not) to Learn React pt. II", tagline: "further adventures", 
+      text: "adding basic static layout using components",
+      additionalInfo: "lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
   ];
 
